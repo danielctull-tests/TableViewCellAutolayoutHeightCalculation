11a28ca56e69d6ded4289259f2af038bd67c7d69
Daniel Tull <dt@danieltull.co.uk>
2013-06-27 21:53:46 +0100

    Initial Commit

---
+++ TableViewCellAutolayoutHeightCalculation/DCTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DCTAppDelegate.h
+//  TableViewCellAutolayoutHeightCalculation
+//
+//  Created by Daniel Tull on 27/06/2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewCellAutolayoutHeightCalculation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewCellAutolayoutHeightCalculation
+//
+//  Created by Daniel Tull on 27/06/2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DCTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DCTAppDelegate class]));
+	}
+}
